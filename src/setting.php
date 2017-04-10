3da36a60c0f22433cb4c389108c00a688dcddf3b
mikeda2501 <mikeda2501@gmail.com>
2017-04-10 18:14:20 +0900

    fix

---
+++ src/setting.php
@@ -0,0 +1,8 @@
+//LineBot
+$channelAccessToken = '[channelAccessToken]';
+$channelSecret = '[channelSecret]';
+
+//GoogleCalendar
+$CALENDAR_ID = '[ID]';
+$API_KEY = '[KEY]';
+$API_URL = 'https://www.googleapis.com/calendar/v3/calendars/'.$CALENDAR_ID.'/events?key='.$API_KEY.'&singleEvents=true';
